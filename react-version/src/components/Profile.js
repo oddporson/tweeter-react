b62432dc819f7a5e630681fb7c29d7475e3cf593
Porson <porsonlee@gmail.com>
2021-11-22 20:14:15 -0800

    removed hardcoded and stored in them into variables

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 function Profile() {
+  const firstName = "Amy"
+  const lastName = "Mansell"
+  const avatar = "/profile-hex.png"
   return(
     <aside>
       <div className="profile">
-        <img className="profile__image" src="./profile-hex.png" alt="person"></img>
+        <img className="profile__image" src={avatar} alt="person"></img>
       </div>
       <br></br>
       <div className="profile__name">
-        <h2><span className="profile--bold">Amy</span> Mansell</h2>
+        <h2><span className="profile--bold">{firstName}</span>{lastName}</h2>
       </div>
   </aside>
   )
