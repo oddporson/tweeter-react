ce633c4e54e925af6cca10c32011058997689b35
Porson <porsonlee@gmail.com>
2021-11-22 21:07:18 -0800

    added event prevent default to form tag

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -4,7 +4,7 @@ function TweetForm() {
   const question = "What are you humming about?"
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" action="/tweets" className="newtweet__form" onClick={(e)=>{e.preventDefault()}}>
         <textarea className="form__textarea" name="text" placeholder={question}></textarea>
         <input type="submit" value="Tweet" className="form__input" onClick={(e)=>e.preventDefault()}></input>
         <span className="form__counter">140</span>
