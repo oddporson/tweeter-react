ec9a8bc4860062244208fd1d05117cab8f0d06e4
Porson <porsonlee@gmail.com>
2021-11-22 21:04:36 -0800

    added onClick button to preventDefault

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -6,7 +6,7 @@ function TweetForm() {
     <section className="newtweet">
       <form method="post" action="/tweets" className="newtweet__form">
         <textarea className="form__textarea" name="text" placeholder={question}></textarea>
-        <input type="submit" value="Tweet" className="form__input"></input>
+        <input type="submit" value="Tweet" className="form__input" onClick={(e)=>e.preventDefault()}></input>
         <span className="form__counter">140</span>
       </form>
     </section>
