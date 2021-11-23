e260f2ca4fb68caab12788a6579ed7fb5c60af0d
Porson <porsonlee@gmail.com>
2021-11-22 20:16:51 -0800

    removed hardcoded and store the string into a variable

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function TweetForm() {
+  const question = "What are you humming about?"
   return (
     <section className="newtweet">
       <form method="post" action="/tweets" className="newtweet__form">
-        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <textarea className="form__textarea" name="text" placeholder={question}></textarea>
         <input type="submit" value="Tweet" className="form__input"></input>
         <span className="form__counter">140</span>
       </form>
